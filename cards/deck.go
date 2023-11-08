c55e7c2f32cc3373ef80e7d64a697e73a159a492
Mehdi Faresi <mfaresi210@gmail.com>
2023-11-08 12:44:17 +0330

    Shuffle a Deck

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of deck
@@ -50,6 +52,18 @@ func newDeckFromFile(filename string) deck {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.saveToFile("mycards")
+	cards.shuffle()
+	cards.print()
 }
