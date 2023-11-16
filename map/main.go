aa3955223d3d2d84a512b33986336998002aced6
Mehdi Faresi <mfaresi210@gmail.com>
2023-11-16 10:31:52 +0330

    Manipulating Maps

--- map/main.go
+++ map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"black": "#000000",
 	}
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
